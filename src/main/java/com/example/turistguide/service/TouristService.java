0d2cf797acc367e282ab5a84127051e9c7d00a18
johanoliverlarsen <johanoliverlarsen@gmail.com>
2026-01-28 11:33:27 +0100

    GetTouristAttractions + getTouristAttractionsByName

--- src/main/java/com/example/turistguide/service/TouristService.java
+++ src/main/java/com/example/turistguide/service/TouristService.java
@@ -1,5 +1,32 @@
 package com.example.turistguide.service;
 
+import com.example.turistguide.model.TouristAttraction;
+import com.example.turistguide.repository.Touristrepository;
+
+import java.util.List;
+
 public class TouristService {
+
+    private final TouristRepository repository;
+
+    public TouristService(TouristRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<TouristAttraction> getTouristAttractions(){
+        return Touristrepository.getTouristAttractions();
+
+    }
+
+    public TouristAttraction getTouristAttractionByName(String name, String caps) {
+        TouristAttraction touristAttraction = repository.getTouristAttractionByName(name);
+        if (caps != null && caps.equals("yes")) {
+            return new TouristAttraction(touristAttraction.getName(), touristAttraction.getDescription().toUpperCase());
+        }
+        return touristAttraction;
+    }
+
+
+
 
 }
