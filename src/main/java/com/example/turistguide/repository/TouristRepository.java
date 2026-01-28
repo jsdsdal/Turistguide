c4d4dc1f57d6f0106da735c4b68f10fc6055b2f1
thistle <thxstle.branch@gmail.com>
2026-01-28 12:24:10 +0100

    mangler controller

--- src/main/java/com/example/turistguide/repository/TouristRepository.java
+++ src/main/java/com/example/turistguide/repository/TouristRepository.java
@@ -19,8 +19,45 @@ public class TouristRepository {
         touristAttractions.add(new TouristAttraction("Rundetårn", "Kulturarv"));
     }
 
+    // getter
     public List<TouristAttraction> getTouristAttractions() { return touristAttractions; }
 
+    // getter til navn
+    public TouristAttraction getTouristAttractionByName(String name) {
+        for (TouristAttraction touristAttraction : touristAttractions) {
+            if (touristAttraction.getName() == name) {
+                return touristAttraction;
+            }
+        }
+        return null;
+    }
 
+    // tilføjer turistattraktion
+    public void addTouristAttraction(TouristAttraction touristAttraction) { touristAttractions.add(touristAttraction); }
+
+    // opdaterer turistattraktion
+    public void updateTouristAttraction (TouristAttraction touristAttraction, String name, String description) {
+        if (name != null){
+            touristAttraction.setName(name);
+        }
+        else if (description != null){
+            touristAttraction.setDescription(description);
+        }
+       }
+
+    // generic remove
+    public void removeTouristAttraction(TouristAttraction touristAttraction) { touristAttractions.remove(touristAttraction); }
+
+    // remove by name
+    public void deleteByName(String name) {
+        TouristAttraction deletedAttraction = touristAttractions.get(0);
+
+        for (TouristAttraction touristAttraction : touristAttractions) {
+            if (touristAttraction.getName() == name) {
+                deletedAttraction = touristAttraction;
+            }
+        }
+        removeTouristAttraction(deletedAttraction);
+    }
 
 }
