2be93143d032e792e04d6490f4a0eab20ed7de35
lindkvst <lindkviste@gmail.com>
2026-01-28 10:36:54 +0100

    Name og Description

--- src/main/java/com/example/turistguide/model/TouristAttraction.java
+++ src/main/java/com/example/turistguide/model/TouristAttraction.java
@@ -1,4 +1,26 @@
 package com.example.turistguide.model;
 
 public class TouristAttraction {
+    //Test indhold
+
+    private String name;
+    private String description;
+
+    public TouristAttraction(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+    public String description() { return description; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Description: " + description;
+    }
+
+
 }
