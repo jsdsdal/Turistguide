fe899ece0216c713d041ce36fb3047b47f5d3ebb
lindkvst <lindkviste@gmail.com>
2026-02-02 11:06:57 +0100

    @Controller

--- src/main/java/com/example/turistguide/controller/TouristController.java
+++ src/main/java/com/example/turistguide/controller/TouristController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("attractions")
 public class TouristController {
     private final TouristService service;
